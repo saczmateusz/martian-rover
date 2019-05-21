c4f5e1bd2bbbb3753ea4c5392fddc0d618112f73
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-05-21 18:59:09 +0200

    platform update

--- szescian/Parts/Platform/Platform.cpp
+++ szescian/Parts/Platform/Platform.cpp
@@ -2,81 +2,96 @@
 
 
 
-Platform::Platform(float x, float y, float z, unsigned int texID, unsigned int smokID)
-	: arm(x, y, z, smokID)
+Platform::Platform(float x, float y, float z)
+	: arm(x, y, z)
 {
-	a[0] = x + 40;
-	a[1] = y + 40;
-	a[2] = z + 30;
-
-	b[0] = x + 75;
-	b[1] = y + 40;
-	b[2] = z + 30;
-
-	c[0] = x + 70;
-	c[1] = y + 70;
-	c[2] = z + 30;
-
-	d[0] = x + 40;
-	d[1] = y + 90;
-	d[2] = z + 30;
-
-	color[0] = 0.8f;
-	color[1] = 0.3f;
-	color[2] = 0.3f;
-
-	LeftFront.setParams(color, a, b, c, d, 5, texID);
-	LeftFront.drawCuboid(0, 0, 0, 0);
-
-	a[0] = x - 5;
-	b[0] = x + 30;
-	c[1] = y + 90;
-	c[0] = x + 30;
-	d[1] = y + 70;
-	d[0] = x;
-	RightFront.setParams(color, a, b, c, d, 5, texID);
-	RightFront.drawCuboid(0, 0, 0, 0);
-
-	a[1] = y - 20;
-	a[0] = x - 13;
-	b[1] = y;
-	b[0] = x + 30;
-	c[1] = y + 35;
-	c[0] = x + 30;
-	d[1] = y + 35;
-	d[0] = x - 5.5f;
-	RightBack.setParams(color, a, b, c, d, 5, texID);
-	RightBack.drawCuboid(0, 0, 0, 0);
-
-	a[0] = x + 83;
-	b[0] = x + 40;
-	c[0] = x + 40;
-	d[0] = x + 75.5f;
-	LeftBack.setParams(color, a, b, c, d, 5, texID);
-	LeftBack.drawCuboid(0, 0, 0, 0);
-
-	GLfloat cl[] = { 0.5f, 0.5f, 0.5f };
-	GLfloat cn[] = { x + 35, y + 40, z + 41 };
-
-	engine.setParams(cl, cn, 15, 30);
-	engine.drawCone(-90, 1, 0, 0);
-
-	cn[1] = y + 10;
-
-	engineCore.setParams(cl, cn, 15, 25, 0, 1);
-	engineCore.drawCylinder(-90, 0, 0, 1);
-
-	cl[0] = 1;
-	cl[1] = 0.4f;
-	cl[2] = 0.3f;
-
-	cn[1] = y - 55;
-
-	thrust.setParams(cl, cn, 10, 40);
-	thrust.drawCone(90, 1, 0, 0);
+	posX = x;
+	posY = y;
+	posZ = z;
 }
 
 
 Platform::~Platform()
 {
+}
+
+void Platform::setTextures(unsigned int texID, unsigned int smokID)
+{
+	banana = texID;
+	arm.setTexture(smokID);
+	a[0] = posX + 40;
+	a[1] = posY + 40;
+	a[2] = posZ + 30;
+
+	b[0] = posX + 75;
+	b[1] = posY + 40;
+	b[2] = posZ + 30;
+
+	c[0] = posX + 70;
+	c[1] = posY + 70;
+	c[2] = posZ + 30;
+
+	d[0] = posX + 40;
+	d[1] = posY + 90;
+	d[2] = posZ + 30;
+
+	color[0] = 0.8f;
+	color[1] = 0.3f;
+	color[2] = 0.3f;
+
+	LeftFront.setParams(color, a, b, c, d, 5, banana);
+
+	a[0] = posX - 5;
+	b[0] = posX + 30;
+	c[1] = posY + 90;
+	c[0] = posX + 30;
+	d[1] = posY + 70;
+	d[0] = posX;
+	RightFront.setParams(color, a, b, c, d, 5, banana);
+
+	a[1] = posY - 20;
+	a[0] = posX - 13;
+	b[1] = posY;
+	b[0] = posX + 30;
+	c[1] = posY + 35;
+	c[0] = posX + 30;
+	d[1] = posY + 35;
+	d[0] = posX - 5.5f;
+	RightBack.setParams(color, a, b, c, d, 5, banana);
+
+	a[0] = posX + 83;
+	b[0] = posX + 40;
+	c[0] = posX + 40;
+	d[0] = posX + 75.5f;
+	LeftBack.setParams(color, a, b, c, d, 5, banana);
+
+	GLfloat cl[] = { 0.5f, 0.5f, 0.5f };
+	GLfloat cn[] = { posX + 35, posY + 40, posZ + 41 };
+
+	engine.setParams(cl, cn, 15, 30);
+
+	cn[1] = posY + 10;
+
+	engineCore.setParams(cl, cn, 15, 25, 0, 1);
+
+	cl[0] = 1;
+	cl[1] = 0.4f;
+	cl[2] = 0.3f;
+
+	cn[1] = posY - 55;
+
+	thrust.setParams(cl, cn, 10, 40);
+}
+
+
+void Platform::drawPlatform()
+{
+	
+	LeftFront.drawCuboid(0, 0, 0, 0);
+	RightFront.drawCuboid(0, 0, 0, 0);
+	RightBack.drawCuboid(0, 0, 0, 0);
+	LeftBack.drawCuboid(0, 0, 0, 0);
+	engine.drawCone(-90, 1, 0, 0);
+	engineCore.drawCylinder(-90, 0, 0, 1);
+	thrust.drawCone(90, 1, 0, 0);
 }
